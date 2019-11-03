6babdcb0cc3d66ef4c08f9d7f8d5adf9078b8111
UgooP <ugo.prenat@gmail.com>
2019-11-03 14:47:25 +0100

    header + logo ok (index)

---
+++ js/hamburger.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector('.hamburger')
+
+hamburger.addEventListener('click', function() {
+  const top_span = document.querySelector('.top_span')
+  const bottom_span = document.querySelector('.bottom_span')
+  const middle_span = document.querySelector('.middle_span')
+
+  middle_span.classList.toggle('delete_span')
+  top_span.classList.toggle('is_close_top_span')
+  bottom_span.classList.toggle('is_close_bottom_span')
+})
